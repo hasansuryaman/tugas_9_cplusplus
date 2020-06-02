35fe8461040864987019858079f99fc7d799ba3a
hasansuryaman <hasan.suryaman@outlook.com>
2020-06-02 09:15:24 +0700

    Tugas 9 C++

---
+++ tugas09.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int x, z=0;
+    cout <<"Inputkan Nilai X : ";
+    cin >>x;
+    for(int i=1;i<=x;i++){
+        cout <<z <<endl;
+        z = z + 5;
+    }
+}
